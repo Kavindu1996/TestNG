92f0397e7f5d804a5e4e16356ad4f1c8ab687b5b
Kavindu 1996 <kavindugeeshan443@gmail.com>
2022-08-10 20:38:37 +0530

    TestNG samples

---
+++ TestCases.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    void first()
+    {
+        System.out.println("I am inside first");
+    }
+
+    @Test
+    void second()
+    {
+        System.out.println("I am inside second");
+    }
+
+    @Test
+    void third()
+    {
+        System.out.println("I am inside third");
+    }
+}
