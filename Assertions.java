ff7fa3d84b2a5995504b9707573b4fbef2110db8
Kavindu 1996 <kavindugeeshan443@gmail.com>
2022-08-14 21:02:02 +0530

    Assertions

---
+++ Assertions.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Assertions {
+
+    WebDriver driver;
+
+    @BeforeClass
+    void setup()
+    {
+        System.setProperty("webdriver.chrome.driver","D:\\Quality Assurance\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+
+
+
+    @Test(priority = 1)
+    void logoTest()
+    {
+        WebElement logo = driver.findElement(By.xpath("//*[@id=\"divLogo\"]/img"));
+        Assert.assertTrue(logo.isDisplayed(),"Logo not displayed");
+    }
+
+    @Test(priority = 2)
+    void homePageTitle()
+    {
+        String title = driver.getTitle();
+        Assert.assertEquals("OrangeHRM",title,"Title is not matched");
+    }
+
+    @AfterClass
+    void tearDown()
+    {
+        driver.quit();
+    }
+
+}
